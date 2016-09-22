8f927ac24daab273bac435778fc7666a8a2374fa
Keith <li.liang@lifecycle.cn>
2016-09-22 09:40:42 +0800

    Initial Commit

---
+++ Face/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Face
+//
+//  Created by Keith on 16/9/22.
+//  Copyright © 2016年 Keith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Face/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Face
+//
+//  Created by Keith on 16/9/22.
+//  Copyright © 2016年 Keith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
